f6005175bf3b1a56ecfc0cd94141f31185385272
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-25 20:57:13 -0300

    working on logos

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <section className="top-section">
+          <img src={logo} alt="logo" width="300" height="100" />
+        </section>
+        <nav className="top-menu">
+          <ul>
+            <li>
+              <a href="#top-menu">Home</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
