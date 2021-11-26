1d56a64fe72d5aba11cdbc7417a723a65eb0f83d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-25 22:24:00 -0300

    working on header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import info from './assets/info.png';
+import attendance from './assets/attendance.png';
+import insta from './assets/insta.png';
 import './App.css';
 
 function App() {
@@ -7,11 +10,26 @@ function App() {
       <header className="header">
         <section className="top-section">
           <img src={logo} alt="logo" width="300" height="100" />
+          <img src={info} alt="info" width="300" height="100" />
+          <img src={attendance} alt="info" width="300" height="100" />
+          <img src={insta} alt="info" width="300" height="100" />
         </section>
         <nav className="top-menu">
           <ul>
             <li>
-              <a href="#top-menu">Home</a>
+              <a href="#top-menu">INICIO</a>
+            </li>
+            <li>
+              <a href="#top-menu">ESPECIALIDADES</a>
+            </li>
+            <li>
+              <a href="#top-menu">CONVÊNIOS</a>
+            </li>
+            <li>
+              <a href="#top-menu">SOBRE NÓS</a>
+            </li>
+            <li>
+              <a href="#top-menu">CONTATO</a>
             </li>
           </ul>
         </nav>
