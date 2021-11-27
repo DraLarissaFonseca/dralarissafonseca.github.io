98598578eb46f892063c8a8fe5754a1ba8e5a126
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-27 16:41:11 -0300

    content added to page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="header">
+      <header className="header" id="top-menu">
         <section className="top-section">
           <img src={logo} alt="logo" width="300" height="100" />
           <img src={info} alt="info" width="300" height="100" />
@@ -61,18 +61,32 @@ function App() {
           </p>
         </div>
       </div>
+      <div className="pics-top" data-negative="false">
+        <svg xmlns="http://www.w3.org/2000/svg" className="border-top-svg"  viewBox="0 0 1000 100" preserveAspectRatio="none">
+          <path opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"/>
+          <path opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"/>
+          <path d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"/>
+        </svg>
+      </div>
       <div id="especialidades">
-        <div className="especialties-list">
-          <h2>Especialidades</h2>
+          <h2>ESPECIALIDADES</h2>
+        <div className="specialties-list">
           <ul>
             <li>Fisioterapia</li>
             <li>Pilates</li>
             <li>RPG</li>
           </ul>
         </div>
+        
       </div>
+      <div className="pics-bottom">
+          <svg xmlns="http://www.w3.org/2000/svg" className="border-bottom-svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+            <path opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"/>
+            <path opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"/>
+            <path d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"/>	
+          </svg>
+      </div> 
       <div id="contato">
-        <h2>Onde nos encontrar</h2>
         <div class="mapouter">
           <div class="gmap_canvas">
             <iframe title="maps" width="600" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=condominio%20sonho%20de%20vida%20araruama&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
@@ -80,14 +94,41 @@ function App() {
             <a href="https://www.embedgooglemap.net">google maps iframe code</a>
           </div>
         </div>
-        <div className="contacts">
-          <h2>Contatos</h2>
-          <p>Telefone: (21) 981008743</p>
-          <p>Whatsapp: (21) 981008743</p>
-          <p>E-mail: larissatf@yahoo.com.br</p>
+        <div className="infos">
+          <div className="where">
+            <h3>ONDE NOS ENCONTRAR</h3>
+            <p>Rodovia Amaral Peixoto, km 80, Ponte dos Leites – Araruama, Rio de Janeiro. Brasil</p>
+          </div>
+          <div className="contacts">
+            <h3>CONTATOS</h3>
+            <p>Telefone: (21) 994208886</p>
+            <p>Whatsapp: (21) 994208886</p>
+            <p>E-mail: larissatf@yahoo.com.br</p>
+          </div>
         </div>
-
       </div>
+      <footer>
+        <nav className="top-menu">
+          <div className="top-menu-div">
+          <ul>
+            <li>
+              <a href="#top-menu">INICIO</a>
+            </li>
+            <li>
+              <a href="#especialidades">ESPECIALIDADES</a>
+            </li>
+            <li>
+              <a href="#quem-sou">QUEM SOU</a>
+            </li>
+            <li>
+              <a href="#contato">CONTATO</a>
+            </li>
+          </ul>
+          </div>
+        </nav>
+          <p>Este site foi criado por <a href="williamguilhermesouza.github.io">William Souza</a></p>
+          <p>Copyright © 2021. Todos os direitos reservados.</p>
+      </footer>
     </div>
   );
 }
